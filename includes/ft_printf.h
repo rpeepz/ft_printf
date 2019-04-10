556187e209c9f75f5393e814cdf060f34a7e56b6
Robert Papagna <rpapagna@e1z2r8p13.42.us.org>
2019-04-10 02:13:30 -0700

    changes, added a new struct

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -16,10 +16,9 @@
 # include <stdarg.h>
 
 # define IF_THEN(X, Y) if(X) {(Y);}
-# define BREAK(X, Y) if(X) {(Y); break ;}
+# define IF_RETURN(X, Y) if(X) {return (Y);}
 
 int				ft_printf(char *str, ...);
-
 /*
 ** STRUCTURES
 */
@@ -41,20 +40,35 @@ typedef struct	s_mods
 	int			length;
 }				t_mods;
 
+typedef struct	s_dispatch_specifier
+{
+	char		specifier;
+	int			(*f)();
+}				t_dispatch_specifier;
 /*
-** GETTER FUNCTIONS
+** GETTING AND SETTING FUNCTIONS
 */
-t_mods			get_mods(char *str, va_list ap);
-/*
-** DOING FUNCTIONS
-*/
+
 void			set_flags(t_flag *flags, char c);
 void			print_one(char **str);
 void			errors(int err_num, char **str);
-/*
-** FINDER FUNCTIONS
-*/
-
+t_mods			get_mods(char *str, va_list ap);
 int				is_flag(char x);
 int				get_type_specifier(char x);
+/*
+** CONVERSION FUNCTIONS
+*/
+
+int				convert_percent(t_mods modifiers);
+int				convert_d(t_mods modifiers, va_list ap);
+int				convert_c(t_mods modifiers, va_list ap);
+int				convert_s(t_mods modifiers, va_list ap);
+int				convert_p(t_mods modifiers, va_list ap);
+int				convert_o(t_mods modifiers, va_list ap);
+int				convert_u(t_mods modifiers, va_list ap);
+int				convert_x(t_mods modifiers, va_list ap);
+int				convert_f(t_mods modifiers, va_list ap);
+int				convert_k(t_mods modifiers, va_list ap);
+int				convert_help(t_mods modifiers, va_list ap);
+
 #endif
