53e3934158e5f2ef6c93899ce1b8175434ec8a09
Robert Papagna <rpapagna@e1z2r8p13.42.us.org>
2019-04-10 02:14:54 -0700

    reworked psudo switch, still optimizing...

--- srcs/finders.c
+++ srcs/finders.c
@@ -19,31 +19,56 @@ int		is_flag(char x)
 
 int		get_type_specifier(char x)
 {
-	if (x == '%')
-		return (1);
-	else if (x == 'd' || x == 'i')
-		return (2);
-	else if (x == 'c')
-		return (3);
-	else if (x == 's')
-		return (4);
-	else if (x == 'p')
-		return (5);
-	else if (x == 'o')
-		return (6);
-	else if (x == 'u')
-		return (7);
-	else if (x == 'x' || x == 'X')
-		return (8);
-	else if (x == 'f' || x == 'F')
-		return (9);
-	else if (x == 'k' || x == 'K')
-		return (10);
-	else if (x == '?')
-		return (11);
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = "%dcspouxfk?";
+	while (str[i])
+	{
+		if (x == str[i])
+			return (i + 1);
+		i++;
+	}
+	str = ".i.....XFK.";
+	while (str[i])
+	{
+		if (x == str[i])
+			return (i + 1);
+		i++;
+	}
 	return (0);
 }
 /*
+** old version revised with help of
+** jmbomeyo
+**int		get_type_specifier(char x)
+**{
+**	if (x == '%')
+**		return (1);
+**	else if (x == 'd' || x == 'i')
+**		return (2);
+**	else if (x == 'c')
+**		return (3);
+**	else if (x == 's')
+**		return (4);
+**	else if (x == 'p')
+**		return (5);
+**	else if (x == 'o')
+**		return (6);
+**	else if (x == 'u')
+**		return (7);
+**	else if (x == 'x' || x == 'X')
+**		return (8);
+**	else if (x == 'f' || x == 'F')
+**		return (9);
+**	else if (x == 'k' || x == 'K')
+**		return (10);
+**	else if (x == '?')
+**		return (11);
+**	return (0);
+**}
+**
 ** will try to incorporate the ability
 ** to print a timestamp using 'k'
 ** possibly mods for formatting
