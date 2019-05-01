441011f0f245e546bfc6a0b85aa3f84010b502a9
Robert Papagna <rpapagna@e1z2r8p23.42.us.org>
2019-04-30 19:06:43 -0700

    skeleton for %p and %f

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 22:34:52 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/04/30 18:56:22 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/04/30 19:05:53 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,11 @@ int				parse_string(char **str, va_list ap)
 	IF_RETURN((i == 2 || i == 12) && (*str)++, convert_i(modifier, ap));
 	IF_RETURN(i == 3 && (*str)++, convert_c(modifier, ap));
 	IF_RETURN(i == 4 && (*str)++, convert_s(modifier, ap));
-//	IF_RETURN(i == 5 && (*str)++, convert_p(modifier, ap));
+	IF_RETURN(i == 5 && (*str)++, convert_p(modifier, ap));
 	IF_RETURN((i == 6 || i == 16) && (*str)++, convert_o(modifier, ap, i));
 	IF_RETURN((i == 7 || i == 17) && (*str)++, convert_u(modifier, ap, i));
 	IF_RETURN((i == 8 || i == 18) && (*str)++, convert_x(modifier, ap, i));
-//	IF_RETURN((i == 9 || i == 19) && (*str)++, convert_f(modifier, ap, i));
+	IF_RETURN((i == 9 || i == 19) && (*str)++, convert_f(modifier, ap, i));
 //	IF_RETURN((i == 10 || i == 20) && (*str)++, convert_k(modifier, ap, i));
 //	IF_RETURN(i == 11 && (*str)++, convert_help(modifier, ap));
 	return (42);
