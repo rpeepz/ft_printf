eda8cb4029ce97b49fc7f6571de2bc53b8d78ae0
Robert Papagna <rpapagna@e1z2r8p23.42.us.org>
2019-05-01 01:15:10 -0700

    more testing outputs acurate to printf with prcsn

--- printf_convert/convert_p.c
+++ printf_convert/convert_p.c
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 21:20:04 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/05/01 00:16:17 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/05/01 01:12:31 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,23 @@ static	int		right_justify(t_mods mod, char *num, int nbyte)
 	int		len;
 
 	len = (int)ft_strlen(num);
-	nbyte = pad_width(mod, len, nbyte, mod.flags.fplus);
-	if (mod.prcsn > len)
-		while ((mod.prcsn--) - len > 0)
-			nbyte += (int)write(1, "0", 1);
+	if ((mod.flags.fzero && mod.width > 14) || mod.prcsn > 12)
+	{
+		nbyte += (int)write(1, "0x", 2);
+		len -= 2;
+		num += 2;
+		nbyte = pad_width(mod, len, nbyte, mod.flags.fplus);
+		if (mod.prcsn > len - 2)
+			while ((mod.prcsn--) - len > 0)
+				nbyte += (int)write(1, "0", 1);
+	}
+	else
+	{
+		nbyte = pad_width(mod, len, nbyte, mod.flags.fplus);
+		if (mod.prcsn > len)
+			while ((mod.prcsn--) - len > 0)
+				nbyte += (int)write(1, "0", 1);
+	}
 	return (nbyte += (int)write(1, num, len));
 }
 
@@ -85,7 +98,7 @@ int				convert_p(t_mods modifiers, va_list ap)
 		tmp = num_string_u_base(input, 16);
 		nbyte = -1;
 		while (tmp[++nbyte])
-			ft_tolower(tmp[nbyte]);
+			tmp[nbyte] = ft_tolower(tmp[nbyte]);
 		addr = ft_strjoin("0x", tmp);
 		free(tmp);
 	}
