2655b97e1996a84d410e35e99a5b2871c39b7247
Robert Papagna <rpapagna@e1z2r8p23.42.us.org>
2019-04-30 21:11:52 -0700

    got addr done time to pad

--- printf_convert/convert_p.c
+++ printf_convert/convert_p.c
@@ -6,13 +6,54 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 21:20:04 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/04/26 21:22:40 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/04/30 21:11:36 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+/*
+**	p		The void * pointer argument is printed in hexadecimal
+**			(as if by `%#x' or `%#lx').
+**			The effect of padding the %p format with zeros (either by the 0 flag
+**			or by specifying a precision), and the benign effect (i.e., none) of
+**			the # flag on %n and %p conversions, as well as other nonsensical
+**			combinations are not standard; such combinations should be avoided.
+*/
+
+static int	right_justify(t_mods mod, char *str, int len, int nbyte)
+{
+	return (nbyte);
+}
+
+static int	left_justify(t_mods mod, char *str, int len, int nbyte)
+{
+	return (nbyte);
+}
+
 int			convert_p(t_mods modifiers, va_list ap)
 {
-	return (1);
+	intptr_t	input;
+	char		*tmp;
+	char		*addr;
+	int			nbyte;
+
+	input = va_arg(ap, intptr_t);
+	if (input)
+	{
+		tmp = num_string_u_base(input, 16);
+		nbyte = -1;
+		while (tmp[++nbyte])
+			ft_tolower(tmp[nbyte]);
+		addr = strjoin("0x", tmp);
+		free(tmp);
+	}
+	else
+		addr = ft_strdup("0x0");
+	if (modifiers.flags.minus == 1)
+		nbyte = left_justify(modifiers, addr, (int)ft_strlen(addr), 0);
+	else
+		nbyte = right_justify(modifiers, addr, (int)ft_strlen(addr), 0);
+	free(addr);
+	return (nbyte);
 }
