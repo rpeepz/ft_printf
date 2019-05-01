8a1f554be5159c7575f0b7d9e8acfb86e83461b5
Robert Papagna <rpapagna@e1z2r8p23.42.us.org>
2019-05-01 00:10:55 -0700

    outlines for long double to a

--- printf_convert/convert_f.c
+++ printf_convert/convert_f.c
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 19:04:29 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/04/30 21:13:22 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/05/01 00:09:02 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,38 @@
 **          If a decimal point appears, at least one digit appears before it.
 */
 
-int			convert_f(t_mods modifiers, va_list ap, int i)
+static int			right_justify(t_mods mod, char **str, int nbyte, int len)
 {
-	return (1);
+	return (nbyte);
+}
+
+static int			left_justify(t_mods mod, char **str, int nbyte, int len)
+{
+	return (nbyte);
+}
+
+int					convert_f(t_mods modifiers, va_list ap, int i)
+{
+	long double		num;
+	char			**str;
+	int				nbyte;
+
+	if (modifiers.length)
+	{
+		if (modifiers.length == 'L' || i == 19)
+			num = va_arg(ap, long double);
+		else if (modifiers.length == 'l' || modifiers.length / 2 == 'l')
+			num = va_arg(ap, double);
+	}
+	else
+		num = va_arg(ap, float);
+	str = num_string_modld(num, modifiers);
+	if (modifiers.flags.minus == 1)
+		nbyte = left_justify(modifiers, str, nbyte,
+		(ft_strlen(str[0]) + ft_strlen(str[1]) + 1));
+	else
+		nbyte = right_justify(modifiers, str, nbyte,
+		(ft_strlen(str[0]) + ft_strlen(str[1]) + 1));
+	ft_pipewrench("--s", str);
+	return (nbyte);
 }
